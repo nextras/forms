6766df2a7ab26270d8e323c5fdffb18d3110849c
hrach <hrach.cz@gmail.com>
2013-01-24 00:52:18 +0100

    MultiOptionList: added server validation

--- Nextras/Forms/Controls/MultiOptionList.php
+++ Nextras/Forms/Controls/MultiOptionList.php
@@ -12,6 +12,8 @@ namespace Nextras\Forms\Controls;
 
 use Nette;
 use Nette\Utils\Html;
+use Nette\Forms\Form;
+use Nette\Forms\IControl;
 
 
 
@@ -24,6 +26,13 @@ use Nette\Utils\Html;
  */
 class MultiOptionList extends OptionList
 {
+	/** validator */
+	const FILLED = ':listFilled';
+
+	/** @var array */
+	protected $value = array();
+
+
 
 	public function __construct($label = NULL, array $items = NULL)
 	{
@@ -36,7 +45,7 @@ class MultiOptionList extends OptionList
 	public function setValue($values)
 	{
 		$this->value = array();
-		foreach ($values as $value) {
+		foreach ((array) $values as $value) {
 			if (isset($this->items[$value])) {
 				$this->value[] = $value;
 			}
@@ -46,14 +55,23 @@ class MultiOptionList extends OptionList
 
 
 
-	/**
-	 * Returns selected radio value. NULL means nothing have been checked.
-	 *
-	 * @return mixed
-	 */
-	public function getValue()
+	public function isFilled()
 	{
-		return is_array($this->value) ? $this->value : NULL;
+		return count($this->getValue()) > 0;
+	}
+
+
+
+	public function addRule($operation, $message = NULL, $arg = NULL)
+	{
+		return parent::addRule($operation === Form::FILLED ? static::FILLED : $operation, $message, $arg);
+	}
+
+
+
+	public function addCondition($operation, $value = NULL)
+	{
+		return parent::addCondition($operation === Form::FILLED ? static::FILLED : $operation, $value);
 	}
 
 
@@ -63,6 +81,17 @@ class MultiOptionList extends OptionList
 		$control = parent::createInputPrototype();
 		$control->name .= '[]';
 		return $control;
+	}
+
+
+
+	/********************* validation *******************/
+
+
+
+	public static function validateListFilled(IControl $control)
+	{
+		return $control->isFilled();
 	}
 
 }
