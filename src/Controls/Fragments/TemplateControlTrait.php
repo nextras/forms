27876a1f958d964a4bf0d36677ffc27e4fb0c0c6
hrach <hrach.cz@gmail.com>
2014-07-21 22:48:11 +0200

    TemplateControlTrait: fixed compatibility with Nette 2.2 [closes #15]

--- src/Controls/Fragments/TemplateControlTrait.php
+++ src/Controls/Fragments/TemplateControlTrait.php
@@ -12,7 +12,10 @@ namespace Nextras\Forms\Controls\Fragments;
 
 use Nette;
 use Nette\Application\UI\IRenderable;
+use Nette\Application\UI\ITemplate;
+use Nette\Application\UI\ITemplateFactory;
 use Nette\Application\UI\Presenter;
+use Nette\Bridges\ApplicationLatte\Template;
 
 
 /**
@@ -89,11 +92,11 @@ trait TemplateControlTrait
 	/**** Application\UI\Control **************************************************************************************/
 
 
-	/** @var Nette\Templating\ITemplate */
+	/** @var ITemplate */
 	private $template;
 
 	/** @var array */
-	private $invalidSnippets = array();
+	private $invalidSnippets = [];
 
 	/** @var bool */
 	public $snippetMode;
@@ -103,17 +106,20 @@ trait TemplateControlTrait
 
 
 	/**
-	 * @return Nette\Templating\ITemplate
+	 * @return ITemplate
 	 */
 	final public function getTemplate()
 	{
 		if ($this->template === NULL) {
 			$value = $this->createTemplate();
-			if (!$value instanceof Nette\Templating\ITemplate && $value !== NULL) {
-				$class2 = get_class($value); $class = get_class($this);
-				throw new Nette\UnexpectedValueException("Object returned by $class::createTemplate() must be instance of Nette\\Templating\\ITemplate, '$class2' given.");
+			if (!$value instanceof ITemplate && $value !== NULL) {
+				$class2 = get_class($value);
+				$class = get_class($this);
+				throw new Nette\UnexpectedValueException("Object returned by $class::createTemplate() must be instance of Nette\\Application\\UI\\ITemplate, '$class2' given.");
 			}
 			$this->template = $value;
+			$this->template->form = $this->template->_form = $this;
+			$this->template->setFile($this->templateFile ?: dirname($this->reflection->getFileName()) . '/' . $this->reflection->getShortName() . '.latte');
 		}
 		return $this->template;
 	}
@@ -121,32 +127,21 @@ trait TemplateControlTrait
 
 	/**
 	 * @param  string|NULL
-	 * @return Nette\Templating\ITemplate
-	 * @modified Jan Skrasek
+	 * @return ITemplate
 	 */
 	protected function createTemplate($class = NULL)
 	{
-		$templateFile = $this->templateFile ?: dirname($this->reflection->getFileName()) . '/' . $this->reflection->getShortName() . '.latte';
-
-		$template = $class ? new $class : new Nette\Templating\FileTemplate($templateFile);
-		$presenter = $this->getPresenter(FALSE);
-		$template->onPrepareFilters[] = $this->templatePrepareFilters;
-		$template->registerHelperLoader('Nette\Templating\Helpers::loader');
-
-		// default parameters
+		$template = $this->getPresenter()->getTemplateFactory()->createTemplate($this->getPresenter());
 		$template->control = $template->_control = $this;
-		$template->presenter = $template->_presenter = $presenter;
-		$template->form = $template->_form = $this;
-		if ($presenter instanceof Presenter) {
-			$template->setCacheStorage($presenter->getContext()->getService('nette.templateCacheStorage'));
-			$template->user = $presenter->getUser();
-			$template->netteHttpResponse = $presenter->getContext()->getByType('Nette\Http\IResponse');
-			$template->netteCacheStorage = $presenter->getContext()->getByType('Nette\Caching\IStorage');
-			$template->baseUri = $template->baseUrl = rtrim($presenter->getContext()->getByType('Nette\Http\IRequest')->getUrl()->getBaseUrl(), '/');
-			$template->basePath = preg_replace('#https?://[^/]+#A', '', $template->baseUrl);
-		}
-		if (!isset($template->flashes) || !is_array($template->flashes)) {
-			$template->flashes = array();
+		$template->flashes = [];
+
+		if ($template instanceof Template) {
+			$latte = $template->getLatte();
+			if ($latte->onCompile instanceof \Traversable) {
+				$latte->onCompile = iterator_to_array($latte->onCompile);
+			}
+
+			$this->templatePrepareFilters($template);
 		}
 
 		return $template;
@@ -155,12 +150,11 @@ trait TemplateControlTrait
 
 	/**
 	 * Descendant can override this method to customize template compile-time filters.
-	 * @param  Nette\Templating\Template
+	 * @param  ITemplate
 	 * @return void
 	 */
 	public function templatePrepareFilters($template)
 	{
-		$template->registerFilter($this->getPresenter()->getContext()->createService('nette.latte'));
 	}
 
 
@@ -186,7 +180,7 @@ trait TemplateControlTrait
 	public function validateControl($snippet = NULL)
 	{
 		if ($snippet === NULL) {
-			$this->invalidSnippets = array();
+			$this->invalidSnippets = [];
 
 		} else {
 			unset($this->invalidSnippets[$snippet]);
@@ -206,7 +200,7 @@ trait TemplateControlTrait
 				return TRUE;
 
 			} else {
-				$queue = array($this);
+				$queue = [$this];
 				do {
 					foreach (array_shift($queue)->getComponents() as $component) {
 						if ($component instanceof IRenderable) {
